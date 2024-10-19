37da43846441d9557598257586b812152c878ffd
Heather <heatherboden3@gmail.com>
2024-10-18 21:28:19 -0400

    adding files to backend assignment 2

---
+++ Assignment2-Heather-Boden/Controllers/Q1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2_Heather_Boden.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Q1 : ControllerBase
+    {
+        ///<summary>
+        ///Gain 50 points for every package delivered
+        /// lose 10 points for every collision with an obstacle.
+        /// earn a bonus 500 points if the number of packages delivered is greater than the number of collisions with the obstacles. 
+        ///  task: determine the final score at the end of a game. 
+        /// </summary>
+        /// 
+        /// <returns>
+        /// 
+        /// </returns>
+        /// 
+        /// <example>
+        /// 
+        /// </example>
+        [HttpPost(template: "delivedroid")]
+        public int delivedroid([FromForm] int numCollision, [FromForm] int numDelivered)
+        {
+            //points for delivered package
+            int deliveredPoints = 50;
+            //loose points for collision with obstacle.
+            int collisionPoints = 10;
+            //bonus if num packages del. is greater than the collisions. 
+            int bonus = 500;
+
+
+            int deliveries = numDelivered * deliveredPoints;
+            int collisions = numCollision * collisionPoints;
+            //bool earnBonus = deliveries >= collisions;
+
+            int finalScore = deliveries - collisions;
+            if (numDelivered > numCollision)
+            {
+               finalScore +=  bonus;
+            }
+
+            return finalScore;
+        }
+        // determine final score at the end of a game:
+        // use an if statement to calculate if the delivered is >= the collisons. 
+        //
+    }
+}
---
+++ Assignment2-Heather-Boden/Controllers/Q4.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2_Heather_Boden.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Q4 : ControllerBase
+    {
+        //Question Selected : 2015 J2 Happy or Sad
+        /// <summary>
+        /// asseses the emoticon within a message and determines if the user is expressing, happy, sad or none. 
+        /// </summary>
+        /// <returns>
+        /// 
+        /// </returns>
+        /// <example>
+        /// 
+        /// </example>
+        [HttpGet(template:"HappySad/{textMessage}")]
+        public string HappySad(string textMessage)
+        {
+            string happy = ":-)";
+            string sad = ":-(";
+
+            int happyCount = 0;
+            int sadCount = 0;
+
+            happyCount=textMessage.Split(happy).Length - 1;
+            sadCount = textMessage.Split(sad).Length - 1;
+
+            //none
+            if (happyCount == 0 && sadCount == 0)
+            {
+                return "none";
+            }
+            //equal amount of happy & sad:
+            else if (happyCount == sadCount)
+            {
+                return "unsure";
+            }
+
+            //happy < no. of sad
+            else if(happyCount > sadCount)
+            {
+                return "happy";
+            }
+            //no of sad < no. of happy
+            else if(happyCount < sadCount) 
+            {
+                return "sad";
+            }
+            return "none";
+        }
+    }
+}
